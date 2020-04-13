2f93feabaefe9ed83b282e88e635779a55b3a015
sujae815 <sujae815@yandex.com>
2020-04-13 05:09:16 +0200

    structure

---
+++ pro_hiring/src/components/PaymentMethodDetails.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container">
+    <h1 class="title">{{ title }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PaymentMethodDetails",
+  props: {
+    title: String
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.container {
+  display: block;
+  padding-left: 60px;
+}
+.container .title {
+  text-align: left;
+}
+</style>
